6d5d46a0a6ebc661d66395032ed8dc471a1eb381
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2023-05-22 21:09:19 +0300

    refactor(DB): user name limit

--- src/main/resources/db/migration/V0_4__Create_table_user.sql
+++ src/main/resources/db/migration/V0_4__Create_table_user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE if not exists "user" (
     id_user uuid primary key default uuid_generate_v4(),
     group_id uuid references "group"(id_group),
-    first_name varchar(50),
-    last_name varchar(50),
-    username varchar(30),
+    first_name varchar,
+    last_name varchar,
+    username varchar,
     password varchar,
     email varchar,
     role role,
