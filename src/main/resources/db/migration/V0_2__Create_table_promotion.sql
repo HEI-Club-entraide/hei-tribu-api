cdf79d42f5008e05bd0227fadf4e640260d352ae
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-02 17:48:19 +0300

    refactor(DB): change name of id of each table

--- src/main/resources/db/migration/V0_2__Create_table_promotion.sql
+++ src/main/resources/db/migration/V0_2__Create_table_promotion.sql
@@ -1,5 +1,5 @@
 CREATE TABLE  if not exists "promotion" (
-    id_promotion uuid primary key default uuid_generate_v4(),
+    id uuid primary key default uuid_generate_v4(),
     name varchar,
     promotion_begin timestamp with time zone not null default now(),
     promotion_end timestamp,
