b3803eacb04f0ebeefdef8c449fa035ec45ec72d
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-20 22:11:39 +0300

    feat: the glogbal security filter chain

---
+++ src/main/java/com/hei/tribu/conf/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.hei.tribu.conf.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Bean;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+    //TODO: implement the jwt config
+}
