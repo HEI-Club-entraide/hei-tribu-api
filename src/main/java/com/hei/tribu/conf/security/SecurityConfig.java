4c5fb158bc6aa363c7a3fff4348548b8a7d2ada9
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-06-21 16:55:17 +0300

    refactor: bean annotation in wewSecurityFilterChain

--- src/main/java/com/hei/tribu/conf/security/SecurityConfig.java
+++ src/main/java/com/hei/tribu/conf/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.context.annotation.Bean;
 
 @EnableWebSecurity
 public class SecurityConfig {
